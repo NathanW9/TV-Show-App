7eb362011b5c157dc098799609c48fa2114fa354
Nathan Waychoff <nwaychoff@ithaca.edu>
2023-09-07 19:08:54 -0400

    Lab 2 Complete

--- TvShow.py
+++ TvShow.py
@@ -1,12 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
+from app import app
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,22 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 
+
 @app.route('/')
-@app.route('/index')
+@app.route('/main')
+def main():
+    return render_template("main.html", title='Home')
+
+@app.route('/show_list')
+def show_list():
+    list_shows = ["Money Heist", "The 100", "Prison Break", "Breaking Bad"]
+    return render_template("show_list.html", title="Tv Show", show_list=list_shows)
+
+@app.route('/money_heist')
+def show_page():
+    return render_template("money_heist.html", title="Money Heist")
+
+
+@app.route('/new_show')
+def new_show():
+    return render_template("new_show.html", title="New Show")
